49b3485a20bc36f9ca4e44d3d4b9ad71c6849cf8
Ajinkya243 <ajinkyagund2@gmail.com>
2025-06-17 13:45:17 +0530

    feat: add TMDB api for fetch

---
+++ src/hooks/useNowPlayingMovies.jsx
@@ -0,0 +1,19 @@
+import { API_OPTIONS } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/moviesSlice";
+import { useEffect } from "react"
+const useNowPlayingMovies=()=>{
+    const dispatch=useDispatch();
+  useEffect(()=>{
+    const getNowPlaying=async()=>{
+      const response=await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS)
+      const json=await response.json();
+      console.log(json.results);
+      dispatch(addNowPlayingMovies(json.results));
+    }
+    getNowPlaying();
+    
+  },[]);
+}
+
+export default useNowPlayingMovies;
